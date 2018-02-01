f1d10213fa99927f6abbd4a361831219c217f263
Siddharth Pant <siddharthpant92@gmail.com>
2018-02-01 16:45:05 -0700

    Exercise 10 done

---
+++ Exercise 10/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+                <button v-custom-on="clicked">Custom click directive</button>
+
+                <br><br>
+                Note: See instructor example for this!!
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        directives:
+        {
+            'custom-on':
+            {
+                bind(el, binding, vnode)
+                {
+                    el.addEventListener('click', function()
+                    {
+                        binding.value();
+                    });
+                }
+            }
+        },
+        methods:
+        {
+            clicked: function()
+            {
+                alert('Custom on-click directive created!!!!')
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ Section 12/src/main.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.directive('highlight', {
+	bind(el, binding, vnode)
+	{
+		var delay = 0;
+		if(binding.modifiers['delayed'])
+		{
+			delay = 3000;
+		}
+		setTimeout(() =>
+		{
+			if(binding.arg == 'background')
+			{
+				el.style.background = binding.value;
+			}
+			else
+			{
+				el.style.color = binding.value;
+			}
+		}, delay)
+	}
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
