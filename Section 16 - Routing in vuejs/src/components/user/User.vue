2b94c9317d90f2225c8b1dcc90899c33121da680
Siddharth Pant <siddharthpant92@gmail.com>
2018-02-04 15:17:40 -0700

    Section 16: till Lecture 232 done

---
+++ Section 16 - Routing in vuejs/src/components/user/User.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+	    <h1>The User Page</h1>
+	    <hr>
+	    <p>Loaded Id : {{id}}</p>
+	    <button @click="navigateToHome" class="btn btn-primary">Go Home</button>
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default
+	{
+		data()
+		{
+			return {
+				id: this.$route.params.id
+			}
+		},
+		watch:
+		{
+			'$route'(to, from)
+			{
+				this.id = to.params.id;
+			}
+		},
+		methods:
+		{
+			navigateToHome: function()
+			{
+				this.$router.push('/');
+			}
+		}
+	}
+</script>
