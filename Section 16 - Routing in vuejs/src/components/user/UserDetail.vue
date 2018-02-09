b2d16a3c1f7b19f0ccff2a6040441e6382f8acae
Siddharth Pant <siddharthpant92@gmail.com>
2018-02-09 15:55:17 -0700

    Named routes for different page layouts

--- Section 16 - Routing in vuejs/src/components/user/UserDetail.vue
+++ Section 16 - Routing in vuejs/src/components/user/UserDetail.vue
@@ -5,7 +5,7 @@
     	<hr>
     	<router-link
     		tag="button"
-    		:to="{name: 'userEdit', params: {id: $route.params.id}}"
+    		:to="{name: 'userEdit', params: {id: $route.params.id}, query: {locale: 'en', q: 100}}"
     		class="btn btn-primary">
     			Edit User {{$route.params.id}}
     	</router-link>
