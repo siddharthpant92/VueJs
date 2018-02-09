148658e159947ec34ce9930fee1078ff7148305a
Siddharth Pant <siddharthpant92@gmail.com>
2018-02-09 15:16:17 -0700

    Set up child routes

--- Section 16 - Routing in vuejs/src/components/user/UserDetail.vue
+++ Section 16 - Routing in vuejs/src/components/user/UserDetail.vue
@@ -1,3 +1,7 @@
 <template>
-    <h3>Some User Details</h3>
+	<div>
+    	<h3>Some User Details</h3>
+    	<p>Loaded id = {{$route.params.id}}</p>
+    	<button @click="$router.push('/user')" class="btn btn-primary">Go to users</button>
+    </div>
 </template>
