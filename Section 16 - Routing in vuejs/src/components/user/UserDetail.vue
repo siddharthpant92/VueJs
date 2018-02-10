06b1380d91e731952f351cdddd38ecf8818f730c
Siddharth Pant <siddharthpant92@gmail.com>
2018-02-10 10:32:47 -0700

    Section 16 done

--- Section 16 - Routing in vuejs/src/components/user/UserDetail.vue
+++ Section 16 - Routing in vuejs/src/components/user/UserDetail.vue
@@ -5,9 +5,28 @@
     	<hr>
     	<router-link
     		tag="button"
-    		:to="{name: 'userEdit', params: {id: $route.params.id}, query: {locale: 'en', q: 100}}"
+    		:to="link"
     		class="btn btn-primary">
     			Edit User {{$route.params.id}}
     	</router-link>
     </div>
 </template>
+
+<script type="text/javascript">
+    export default {
+        data: function()
+        {
+            return {
+                link: {
+                        name: 'userEdit', 
+                        params: {
+                                id: this.$route.params.id
+                            },
+                        query: {
+                                locale: 'en', q: 100
+                            }
+                    }
+            }
+        }
+    }
+</script>
--- Section 16 - Routing in vuejs/src/components/user/UserEdit.vue
+++ Section 16 - Routing in vuejs/src/components/user/UserEdit.vue
@@ -3,5 +3,7 @@
     	<h3>Edit the User</h3>
     	<p>Locale: {{$route.query.locale}}</p>
     	<p>q: {{$route.query.q}}</p>
+    	<div style="height:700px"></div>
+    	<p id="data">Extra data</p>
     </div>
 </template>
