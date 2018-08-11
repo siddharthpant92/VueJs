0237d05855a3633b9f1ffd0155f1856b87cb9903
Siddharth Pant <siddharthpant92@gmail.com>
2018-08-11 16:37:25 -0600

    Created custom component

---
+++ Crash Course/demo/src/components/Posts.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<p>Hello! This is a user created component!</p>
+	</div>
+	
+</template>
+
+
+<script type="text/javascript">
+	export default
+	{
+		name: "Posts",
+		data()
+		{
+			return {
+
+			}
+		}
+
+	}
+</script>
+
+
+<style type="text/css">
+	
+</style>
