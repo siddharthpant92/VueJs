cbddde27eb4a7d0c39158ba1bf720a594ccb9ccb
Siddharth Pant <siddharthpant92@gmail.com>
2018-08-11 18:03:26 -0600

    Set up vue router in main.js

--- Crash Course/demo/src/main.js
+++ Crash Course/demo/src/main.js
@@ -2,12 +2,25 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueRouter from "vue-router"
+import Posts from "./components/Posts"
+Vue.use(VueRouter)
+
+const routes = [
+	{
+		path: '/',
+		component: Posts
+	}
+]
+
+const router = new VueRouter({routes: routes})
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router: router,
   components: { App },
   template: '<App/>'
 })
